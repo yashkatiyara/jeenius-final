4b1b00d9c80a6bd50c1a7782093e3a09f3da7d60
v-khushalani <vkhushalani0001@gmail.com>
2025-10-26 02:13:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ import Login from "./pages/Login";
 import AuthCallback from '@/pages/AuthCallback';
 
 // Feature pages
-import FeaturesPage from "./pages/FeaturesPage";
 import LessonBuilderPage from "./pages/LessonBuilderPage";
 import WhyUsPage from "./pages/WhyUsPage";
 import GoalSelectionPage from '@/pages/GoalSelectionPage';
