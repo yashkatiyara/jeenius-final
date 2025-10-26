77237b3f5929c7c6ba828b4317e56c433b2c940e
v-khushalani <vkhushalani0001@gmail.com>
2025-10-27 02:17:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import FloatingAIButton from '@/components/FloatingAIButton';
 import SubscriptionPlans from '@/pages/SubscriptionPlans';
+import PricingPage from '@/components/Pricing';
 
 
 // Main pages
