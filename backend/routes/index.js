66e0f1550dfbe5441b3b1f2bae4cad515d63ac63
v-khushalani <vkhushalani0001@gmail.com>
2025-11-09 18:41:41 +0530

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studyPlanRoutes = require('./studyPlanRoutes');
-
-// Register study plan routes
-router.use('/study-plan', studyPlanRoutes);
-
-module.exports = router;
